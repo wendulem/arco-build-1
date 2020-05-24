e0d172423bbc65a39637e454eaec8629faef490a
wendulem <34697277+wendulem@users.noreply.github.com>
2020-05-24 00:45:44 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_jsonpify import jsonify
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+pg8000://postgres:abulgLJn9Cexrjvo@127.0.0.1:3306'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.debug = True
+
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+
+# Invoices Class / Model
+
+
+class Invoice(db.Model):
+    __tablename__ = 'invoice'
+    id = db.Column(db.Integer, primary_key=True)
+    contractor = db.Column(db.String(200))
+    invoice_amount = db.Column(db.Float)
+
+    def __init__(self, contractor, invoice_amount):
+        self.contractor = contractor
+        self.invoice_amount = invoice_amount
+
+# Invoices Schema
+
+
+class InvoiceSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'contractor', 'invoice_amount')
+
+
+# Init Schema
+invoice_schema = InvoiceSchema() #no need for strict keyword, schemas always strict
+invoices_schema = InvoiceSchema(many=True)
+
+# Create an Invoice
+@app.route('/invoice', methods=['POST'])
+def add_invoice():
+    contractor = request.json['contractor']
+    invoice_amount = request.json['invoice_amount']
+
+    new_invoice = Invoice(contractor, invoice_amount)
+
+    db.session.add(new_invoice)
+    db.session.commit()
+
+    return invoice_schema.jsonify(new_invoice)
+
+# Get all Invoices
+@app.route('/invoice', methods=['GET'])
+def get_invoices():
+    all_invoices = Invoice.query.all()
+    result = invoices_schema.dump(all_invoices)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run()
